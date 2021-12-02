2f7bd379ca59e006f0b4f5adcb7c6adc1119be92
sumaya <sumayaomarr@gmail.com>
2021-12-01 23:25:31 -0500

    snake game: via zoom

--- client.js
+++ client.js
@@ -1,5 +1,45 @@
 
+const { IP, PORT } = require("./constants");
+const net = require("net");
 
-const net = require("net"); 
-const { IP, PORT } = require("./connect");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('Now connected to the game server.');
+    conn.write("Name: SSS");
+   
+  });
+  conn.on("data", (data) => {
+    // code that does something when reicived data
+    console.log(data, 'data recieved')
+  });
+  conn.on("data", (data) => {
+
+  });
+  // const handleUserInput = function (key) {
+  // if (key === '\u0003') {
+  //   process.exit();
+  // } if (key === 'w') {
+  //   connection.write("Move: up") 
+  // } if (key === 'a') {
+  //   connection.write("Move: left") 
+  // } if (key === 's') {
+  //   connection.write("Move: down") 
+  // } if (key === 'd') {
+  //   connection.write("Move: right") 
+  // } if (key === "f") {
+  //   connection.write("Say: this is so fun")
+  // }}
  
+  return conn;
+};
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,30 +1,17 @@
 
 const net = require("net");
+const {connect} = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  console.log(key)
   
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log('connected successfully!')
-  });
-  
-  conn.on("data", (data) => {
-    // code that does something when reicived data
-    console.log( data,'data recieved')
-  });
-  return conn;
 };
-
 console.log("Connecting ...");
-connect();
 
-
-
+const conn = connect();
+setupInput(conn);
